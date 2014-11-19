fbc292f0125176f88de5a39b8fa0978465fcfa50
ANDREW <Andrew.Barry@gmx.com>
2014-11-19 10:53:19 -0500

    Main menu updated

--- game.py
+++ game.py
@@ -12,21 +12,22 @@ curses.curs_set(0)
 pad = curses.newpad(curses.LINES,curses.COLS) # field
 
 # draw the borders around the screen, fancily
-def drawBorders():
-    horizBorder = '*' * (curses.COLS)
+def drawBorders(char='#'):
+    horizBorder = char * (curses.COLS)
     pad.addstr(0,0,horizBorder)
     for y in range(1,curses.LINES-1):
         pad.refresh(0,0,0,0,curses.LINES,curses.COLS)
         time.sleep(.01)
-        pad.addstr(y,0,'*')
-        pad.addstr(y,curses.COLS-1,'*')
+        pad.addstr(y,0,char)
+        pad.addstr(y,curses.COLS-1,char)
     pad.addstr(curses.LINES-1,0,horizBorder[:-1])
     pad.refresh(0,0,0,0,curses.LINES,curses.COLS)
 
 
 
 # Creates a new window, displays text centered around (y,x) and returns the window for potential usage
-def textWindow(y,x,string):
+# Will make y,x the top left corner if specified
+def textWindow(y,x,string, topLeft=False):
     halfwidth = -1
     halfheight = -1
     linestrings = []
@@ -40,25 +41,52 @@ def textWindow(y,x,string):
     max = len(max)
     halfwidth = (max+4)/2
     halfheight = (len(linestrings)+4)/2
-    next = curses.newwin(4 + len(linestrings),4 + max,y-halfwidth,x-halfheight)
+    if (topLeft):
+        next = curses.newwin(4 + len(linestrings),4 + max,y,x)
+    else:
+        next = curses.newwin(4 + len(linestrings),4 + max, int(y-halfwidth),int(x-halfheight))
     next.box()
     pan = curses.panel.new_panel(next)
     for i in range(0,lines):
         next.addstr(i+2,2,linestrings[i])
     return (pan,next)
-        
 
-
+# do main menu
 def mainMenu(stdscr):
-    str = "ASCIIria Chronicles\n\n1 New Game\n\n2 Select Map\n\n3 Exit"
-    (pan,win) = textWindow(curses.LINES/2,curses.COLS/2,str)
-    pan.top()
+    string = "ASCIIria Chronicles\n\n1 New Game\n\n2 Select Map\n\n3 Exit"
+    (pan,win) = textWindow(curses.LINES/2,curses.COLS/2,string)
     pad.addstr(curses.LINES-2,1,"v0.0.1 by Andrew Barry")
+    pan.top()
+    pad.refresh(0,0,0,0,curses.LINES,curses.COLS)
     win.refresh()
     while 1:
-        time.sleep(0.1)
-
+        c = stdscr.getch()
+        selection = c-48
+        if (selection==1 or selection==2 or selection==3): # return choice
+            return selection
     return -1
+
+# do map selection screen
+def mapSelect():
+    maps = ["Test Map"]
+    display = "SELECT YOUR MAP\n"
+    c = 1
+    for map in maps:
+        display = display + "\n" + str(c) + " " + map + "\n"
+        c += 1
+    display = display + "\n0 Back"
+    (pan,win) = textWindow(10,10,display,topLeft=True)
+    pan.top()
+    win.refresh()
+    while 1:
+        c = stdscr.getch()
+        selection = c-48
+        if (selection <= len(maps) and selection > 0):
+            return maps[selection-1]
+        elif (selection == 0):
+            return "BACK"
+
+
 
 # ends the program
 def closeGame():
@@ -72,7 +100,22 @@ def main(stdscr):
         closeGame()
         return
     drawBorders() # fancy border drawing effect
-    choice = mainMenu(stdscr) # do main menu everything
+    loadgame = True
+    while loadgame: # MAIN MENU LOOP
+        choice = mainMenu(stdscr) # do main menu everything
+        selectedMap = 0
+        if (choice == 3):
+            closeGame()
+            return
+        elif (choice == 1):
+            selectedMap = "Test Map"
+        elif (choice == 2):
+            selectedMap = mapSelect()
+        loadgame = selectedMap is "BACK"
+
+    # load map, select/place units, play game
+
+
     while 1:
         pad.refresh(0,0,0,0,curses.LINES,curses.COLS)
         
